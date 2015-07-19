f77f9f8388f60bd975bda3a2a7e0944ca7e8bdee
Pascal van Kooten <kootenpv@gmail.com>
2015-07-19 14:43:50 +0200

    path again

--- helper.py
+++ helper.py
@@ -1,11 +1,14 @@
 import requests
 import re    
+import os
+
+file_dir = os.path.dirname(os.path.realpath(__file__))
 
 def update_pypi_names():
     html = requests.get('https://pypi.python.org/pypi/?').text
-    with open('/Users/pascal/GDrive/pytrending/pypi_packages.txt', 'w') as f:
+    with open(file_dir + '/pypi_packages.txt', 'w') as f:
         f.write('\n'.join(set([x.lower() for x in re.findall('<a href="/pypi/([^/]+)', html)])))
 
 def get_pypi_names():
-    with open('/Users/pascal/GDrive/pytrending/pypi_packages.txt') as fin:
+    with open(file_dir + '/pypi_packages.txt') as fin:
         return fin.read().split('\n')
